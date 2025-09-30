2682288c9667e5b8392b845233c7e1fbfa400e05
mariosangut <mariosangut@gmail.com>
2025-09-30 14:43:23 +0200

    ejercicio12

---
+++ ejercicio12.py
@@ -0,0 +1,4 @@
+precioBarra = 3.49
+
+barrasVendidas = float(input("¿Cuántas barras se han vendido de pan no fresco? "))
+print (f"El precio de una barra normal es de: {precioBarra}, el descuento es del 60% por no se pan fresco por tanto el precio del pedido será de: {barrasVendidas-barrasVendidas * 0.6}  €" )
