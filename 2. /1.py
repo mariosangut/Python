26e5040cd8cd85ce698f37d6f0e09101453e2696
mariosangut <mariosangut@gmail.com>
2025-10-07 13:45:31 +0200

    Ejercicios

--- 2. /1.py
+++ 2. /1.py
@@ -1,4 +1,4 @@
-nombre = input("¿Cual es su nombre?")
-numero = input("Introduce un numero ")
+nombre = input("¿Cual es su nombre? ")
+numero = int(input("Introduce un numero "))
 
 print((f"{nombre}\n")*numero)
