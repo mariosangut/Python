a21ed5737ea9a1e3180067fd7d70fd5e18b8e87b
mariosangut <mariosangut@gmail.com>
2025-10-08 09:23:50 +0200

    Ejercicio 6

--- 2. /6.py
+++ 2. /6.py
@@ -1,1 +1,7 @@
-prueba
+"""Escribir un programa que pida al usuario que introduzca una 
+frase en la consola y una vocal, y después muestre por pantalla 
+la misma frase pero con la vocal introducida en mayúscula."""
+frase = input("Introduce una frase: ")
+vocal = input("Introduce una vocal: ")
+
+print(f"{frase.replace(vocal, vocal.upper())}")
