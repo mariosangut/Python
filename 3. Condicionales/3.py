9006f982ac96c9e250d82a028663781630d17e40
mariosangut <mariosangut@gmail.com>
2025-10-14 13:48:55 +0200

    Ejercicio 3

---
+++ 3. Condicionales/3.py
@@ -0,0 +1,8 @@
+divisor = int(input("Introduce un numero entero: "))
+dividendo = int(input("Introduce un numero entero: "))
+
+if divisor <0= 0:
+    print ("Error")
+else:
+    resultado = divisor/dividendo 
+    print (f"La division es {resultado}")
