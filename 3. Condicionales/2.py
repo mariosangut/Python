45e0bd78d0fa033897bd6f8f6991fea288eaf08f
mariosangut <mariosangut@gmail.com>
2025-10-14 13:42:10 +0200

    ejercicio 2

---
+++ 3. Condicionales/2.py
@@ -0,0 +1,8 @@
+contrasena = "Malumn0".lower()
+usuario = input("Introduce la contraseña: ").lower()
+
+if (contrasena == usuario):
+    print ("Las contraseñas coinciden")
+else:
+    print ("No coinciden")
+
