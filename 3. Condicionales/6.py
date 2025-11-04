7ae2cf72b5aab29b440aed4263494bc07267adbc
srolest <serafinroldanesteban@gmail.com>
2025-11-04 14:17:20 +0100

    Ejercicio 6

--- 3. Condicionales/6.py
+++ 3. Condicionales/6.py
@@ -1,1 +1,12 @@
-nombre = input("Introduce tu nombre ")
+nombre = input("Introduce tu nombre: ").strip()
+sexo = input("Introduce tu sexo (Masculino / Femenino): ").strip().lower()
+
+primera = nombre[0].upper()
+grupo = 'B'
+
+if sexo.startswith('f') and primera < 'M':
+    grupo = 'A'
+elif sexo.startswith('m') and primera > 'N':
+    grupo = 'A'
+
+print(f"Pertenece al grupo {grupo}")
