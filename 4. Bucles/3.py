ae7365badd3c815b1548b54ddaa3267cf61d553f
mariosangut <mariosangut@gmail.com>
2025-11-11 14:04:12 +0100

    Ejercicio 4

--- 4. Bucles/3.py
+++ 4. Bucles/3.py
@@ -1,9 +1,12 @@
 numero = int(input("Introduce un numero entero positivo: "))
-
 x = 1
 
-while x< numero:
-    if (x %2 != 0):
+while x<numero:
+    if (x %2 != 0 and x<numero-1):
+        print (x, end=",") 
+    elif (x %2 != 0 ):
         print (x)
     
     x+=1
+
+print ()
