a1e6a7429766941280b55e75585fec31dfd6b6b5
Mario Sánchez Gutiérrez <mariosangut@gmail.com>
2026-01-31 21:28:05 +0100

    Correciones

--- 5. Listas y tuplas/6.py
+++ 5. Listas y tuplas/6.py
@@ -1,6 +1,6 @@
 asignaturas, notas = ["Matemáticas", "Física", "Química", "Biología", "Historia"], []
 for a in asignaturas:
-	n = input(f'¿Que has sacado en {a}? '); notas.append(float(n))
+    n = input(f'¿Que has sacado en {a}? '); notas.append(float(n))
 print()
 
 suspensas = [a for a, n in zip(asignaturas, notas) if n < 5.0]
--- 5. Listas y tuplas/8.py
+++ 5. Listas y tuplas/8.py
@@ -1,2 +1,2 @@
-p = input("Introduce una palabra: ");
+p = input("Introduce una palabra: ")
 print(f"La palabra {p} es un palíndromo.") if p == p[::-1] else print(f"La palabra {p} no es un palíndromo.")
